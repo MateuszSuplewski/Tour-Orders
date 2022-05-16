1ef8c9d4b554606add13a8e6a8f5028c84fbf11d
Mateusz Suplewski <matx3582@gmail.com>
2022-05-16 20:18:34 +0200

    Add final transaction, file restriction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,62 @@
-// const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
-// "2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
 
-// console.log(txt.split(/[\r\n]+/gm));
+const fileInputEl = document.querySelector(".uploader__input");
+fileInputEl.addEventListener('change', readFile);
 
-// const splittedData = txt.split('"');
-// //console.log(splittedData);
-// const regrex = /[^,\n]/;
-// const multipleTripData = splittedData.filter(word => word.match(regrex));
-// console.log(multipleTripData);
+function readFile(e) {
+    const file = e.target.files[0]; // Load 1 file
+    if (file && file.type.includes('text')) {
+        const reader = new FileReader();
 
-const fileInput = document.querySelector(".uploader__input");
-fileInput.addEventListener('change', handleFile);
+        reader.onload = function (readerEvent) {
+            const content = readerEvent.target.result; // prepare data to be well formatted
+            const splittedToWords = content.split('"');
+            const goodValuesPattern = /[^,\n\r]/;
+            const excursionsData = splittedToWords.filter(word => word.match(goodValuesPattern));
 
-function handleFile(e) {
+            for (let i = 0; i < excursionsData.length; i += 5) {
+                const id = excursionsData[i]; // prepare data about excursion
+                const name = excursionsData[i + 1];
+                const description = excursionsData[i + 2];
+                const adultPrice = excursionsData[i + 3];
+                const childPrice = excursionsData[i + 4];
 
-    const file = e.target.files[0]; // tylko 1 plik posiadamy 
-    const reader = new FileReader();
+                const excursionPrototypeEl = document.querySelector('.excursions__item:last-of-type'); // Add new excursion
+                const excursionEl = excursionPrototypeEl.cloneNode(true);
+                if (excursionEl.classList.contains('excursions__item--prototype')) excursionEl.classList.remove('excursions__item--prototype');
+                excursionPrototypeEl.parentElement.appendChild(excursionEl);
 
-    reader.onload = function (readerEvent) {
-        const content = readerEvent.target.result;
-        console.log(content);
-
-        const splittedData = content.split('"');
-        //console.log(splittedData);
-        const regrex = /[^,\n\r]/;
-        const multipleTripData = splittedData.filter(word => word.match(regrex));
-
-      
-
-        // dodac tu caly handler ze splitem :)
-            for (let i = 0; i < multipleTripData.length; i += 5) {
-                const id = multipleTripData[i];
-                const name = multipleTripData[i + 1];
-                const description = multipleTripData[i + 2];
-                const adultPrice = multipleTripData[i + 3];
-                const childPrice = multipleTripData[i + 4];
-        
-        
-                const tripPrototype = document.querySelector('.excursions__item:last-of-type');
-                const trip = tripPrototype.cloneNode(true);
-                trip.classList.remove('excursions__item--prototype');
-                tripPrototype.parentElement.appendChild(trip);
-
-                const tripButton = tripPrototype.nextElementSibling.querySelector('.excursions__field-input--submit');
-                trip.dataset.id = id;
-                tripButton.dataset.id=id;
-                
-                const titleEl = tripPrototype.nextElementSibling.querySelector('.excursions__title');
+                const submitButton = excursionEl.querySelector('.excursions__field-input--submit');
+                const titleEl = excursionEl.querySelector('.excursions__title'); // Add data to newly created excursion
+                const descriptionEl = excursionEl.querySelector('.excursions__description');
+                const pricesEl = excursionEl.querySelectorAll('.excursions__price');
+                excursionEl.dataset.id = id; // Add dataset id to easy find which excursion should be added to cart!
+                submitButton.dataset.id = id;
                 titleEl.innerText = name;
-                const descriptionEl =  tripPrototype.nextElementSibling.querySelector('.excursions__description');
                 descriptionEl.innerText = description;
-
-                const pricesEl = tripPrototype.nextElementSibling.querySelectorAll('.excursions__price');
                 pricesEl[0].innerText = adultPrice;
-                pricesEl[1].innerText = childPrice;  
-
+                pricesEl[1].innerText = childPrice;
             }
-    };
-    reader.readAsText(file, 'UTF-8');
+        };
+        reader.readAsText(file, 'UTF-8');
+    }
+    else {
+        console.log('Można dodać handler w przypadku, gdy ktoś wprowadzi zły plik!');
+    }
 }
 
 
-const trips = document.querySelector('.excursions');
-trips.addEventListener('click',addTripToCart);
 
-function addTripToCart(e){
+
+const excursionsEl = document.querySelector('.excursions');
+excursionsEl.addEventListener('click', addTripToCart);
+const basket = [];
+
+
+function addTripToCart(e) {
     e.preventDefault();
 
-    if(e.target.type === 'submit'){
-        
+    if (e.target.type === 'submit') {
+
         const Cart = document.querySelector('.panel__summary');
         const itemPrototype = Cart.querySelector('.summary__item--prototype');
         const newItem = itemPrototype.cloneNode(true);
@@ -89,7 +77,7 @@ function addTripToCart(e){
 
 
         itemTitleEl.innerText = titleEl.innerText;
-        summaryPriceEl.innerText = Number(howMany[0].value)*Number(pricesEl[0].innerText) + Number(howMany[1].value)*Number(pricesEl[1].innerText)+'PLN';
+        summaryPriceEl.innerText = Number(howMany[0].value) * Number(pricesEl[0].innerText) + Number(howMany[1].value) * Number(pricesEl[1].innerText) + 'PLN';
         itemSummaryPricesEl.innerText = `dorośli: ${howMany[0].value} x ${pricesEl[0].innerText}PLN, dzieci: ${howMany[1].value} x ${pricesEl[1].innerText}PLN`;
         newItem.classList.remove('summary__item--prototype');
 
@@ -97,15 +85,21 @@ function addTripToCart(e){
 
         const removeButtonEl = newItem.querySelector('.summary__btn-remove');
         removeButtonEl.addEventListener('click', removeItem);
-        
 
-    } 
 
-  
+
+
+        const totalPriceEl = document.querySelector('.order__total-price-value');
+        let prevValue = totalPriceEl.innerText;
+        let actualValue = summaryPriceEl.innerText;
+        totalPriceEl.innerText = Number(prevValue.replace('PLN', '')) + Number(actualValue.replace('PLN', '')) + 'PLN';
+
+
+    }
 }
 
 
-function removeItem(e){
+function removeItem(e) {
     e.preventDefault();
     console.log(e.target);
     console.log(e.currentTarget);
@@ -113,51 +107,59 @@ function removeItem(e){
     const itemEl = titleEl.parentElement;
 
     itemEl.remove();
+
+    const totalPriceEl = document.querySelector('.order__total-price-value');
+    const price = e.target.previousElementSibling;
+    let prevValue = totalPriceEl.innerText;
+    let actualValue = price.innerText;
+
+    totalPriceEl.innerText = Number(prevValue.replace('PLN', '')) - Number(actualValue.replace('PLN', '')) + 'PLN';
 }
 
 
 const orderEl = document.querySelector('.order');
-orderEl.addEventListener('submit',validateForm);
+orderEl.addEventListener('submit', validateForm);
 
-function validateForm(e){
+function validateForm(e) {
     e.preventDefault();
 
     const nameAndLastName = orderEl.elements[0].value;
     const email = orderEl.elements[1].value;
     const errors = [];
 
-    const nameAndLastNameRegrex =/^[a-ząćęłńóśźż]{2,} [a-ząćęłńóśźż]{2,}$/i;
-    const emailRexgrex=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const nameAndLastNameRegrex = /^[a-ząćęłńóśźż]{2,} [a-ząćęłńóśźż]{2,}$/i;
+    const emailRexgrex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const totalPrice = document.querySelector('.order__total-price-value').innerText;
 
     let ul = e.target.querySelector('ul');
-    
-    if(!ul){
+
+    if (!ul) {
         const ul = document.createElement('ul');
         orderEl.appendChild(ul);
     }
-    
+
     ul = e.target.querySelector('ul');
     const listofItems = ul.querySelectorAll('li');
-    
+
     listofItems.forEach((item) => {
         ul.removeChild(item);
     });
 
 
-    if(!nameAndLastName.match(nameAndLastNameRegrex)){
+    if (!nameAndLastName.match(nameAndLastNameRegrex)) {
         console.log('Nie pasuje nazwa');
         errors.push('Wypełnij pole imie i nazwisko poprawnie.');
     }
-    if(!email.match(emailRexgrex)){
+    if (!email.match(emailRexgrex)) {
         console.log('Nie pasuje email :/');
         errors.push('Wprowadź poprawny adres email.');
     }
 
-    if(errors.length === 0){
-        alert('Dziękujemy za złożenie zamówienia o wartości [kwota zamówienia] PLN. Szczegóły zamówienia zostały wysłane na adres e-mail: adres@wpisanywformularzu.pl.')
+    if (errors.length === 0) {
+        alert(`Dziękujemy za złożenie zamówienia o wartości ${totalPrice}. Szczegóły zamówienia zostały wysłane na adres e-mail: ${email}`);
     }
-    else{
-        errors.forEach((error)=>{
+    else {
+        errors.forEach((error) => {
 
             const li = document.createElement('li');
             ul.appendChild(li);
